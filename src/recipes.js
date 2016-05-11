ead59a4e19a7747f19224f71e69718b942f2dd7c
makpande <mak_pande@yahoo.com>
2016-05-10 21:21:25 -0400

    initial commit

---
+++ src/recipes.js
@@ -0,0 +1,39 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+  "Diabetes": "Diabetes is a disease in which the body is unable to properly use and store glucose (a form of sugar). Glucose backs up in the bloodstream — causing one’s blood glucose (sometimes referred to as blood sugar) to rise too high.",
+  "Types of Diabetes": "Common types of diabetes are Type 1 Diabetes, Type 2 Diabetes, Gestational Diabetes"
+  "Type 1 Diabetes": "In type 1 (fomerly called juvenile-onset or insulin-dependent) diabetes, the body completely stops producing any insulin, a hormone that enables the body to use glucose found in foods for energy.",
+  "Type 2 Diabetes": "Type 2 (formerly called adult-onset or non insulin-dependent) diabetes results when the body doesn’t produce enough insulin and/or is unable to use insulin properly (insulin resistance). This form of diabetes usually occurs in people who are over 40, overweight, and have a family history of diabetes.",
+  "Gestational Diabetes": "Gestational diabetes cause in pregnant women. But those are for temparory period can go away after child birth.",
+  "Eating sweets": "you can have sweets in moderation and as a plan of healthy meals plan.",
+  "Leading cause of death": "Type 2 Diabetes is the seventh leading cause of death in the United States.",
+  "Heart Disease and Diabetes": "It accounts for 90% to 95% of all diagnosed cases of heart disease.",
+  "Role of Insulin in diabetes": "Insulin is a hormone that helps your body turn sugar into energy.",
+  "Insulin resistant diabetes": "when you have type 2 diabetes, your cell don't respond to it like they should.",
+  "Side effects of diabetes": "When it happens, sugar backs up in your blood. Your pancreas makes even more insulin to get rid of sugar, so you wind up with too much of insulin and sugar both.",
+  "Fact about diabetes": "Almost a one third of people with diabetes don't know they have diabetes.",
+  "Common symptoms": "Heavy thirst is common symptoms, people with high blood sugar are often extremely thirsty.Other symptoms include dry mouth, being hungry more (espicially after you eat).",
+  "More symptoms": "peeing more, fatigue, blurred vision, headeahes, itching, numbness and tingling in your hands and feet, wounds heal more slowly, gain weight, frequent yeast infection, have sexual problems.",
+  "Facts about begining of diabetes": "this disease doesn't start suddenly. some people are first diagnosed with prediabetes(also called impaired glucose tolerance or impaired fasting glucose).",
+  "Diabetes control": "lifestyle changes like weight loss, eating a healthier diet, getting more exercise can often ward off full blown diabetes. lifestyle changes is more important can cut your risk by 58%.",
+  "Initial treatment": "Taking prescription medicine like metformin can help, talk to your doctor about treatment.",
+  "Age at risk": "For 60 or older their risk of diabetes is 71%.",
+  "Main factor for diabetes": "Obesity is the main factor people likely to get diabetes",
+  "Severe effects of diabetes": "Over the time Blindness, heart disease, nerve and blood vessel damage, kidney disease and other major health problems",
+  "Common effects on health": "Depression, gum disease also more common for diabetes patient as per CDC.",
+  "Cause for diabetes": "Cause can be genetic and lifestyle, affecting blood glucose level physical activity, illeness, stress",
+  "Effects of type 1 diabetes": "Type 1 diabetes by an auto immune response that kills the body's own insulin producing cells",
+  "Type 1 diabetes treatment": "Treatment of type 1 diabetes is taking insulin daily by injection or insulin pump",
+  "Reduce risk of Diabetes": "By eating healthy meals and snacks and doing regular physical activity, regualar monitoring of blood glucose levels.Execise or physically active everyday of the week for 30 minutes, check feet every six month",
+  "Diagnostic test": "Blood test before meals targeted blood glucose level between 70 to 110 mg/dl and 2 hours after meals upto 140 mg/dl.",
+  "More Diagnostic test": "A1C also know as average blood glucose level of 120 days should have targeted level below 7"
+};
