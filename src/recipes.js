4859b9c7cb30beab5b0c836e26e4e415f0ecab14
makpande <mak_pande@yahoo.com>
2016-05-17 18:04:36 -0400

    change on english grammer and constructions

--- src/recipes.js
+++ src/recipes.js
@@ -11,7 +11,7 @@
 module.exports = {
   "Diabetes": {
     next: "Types of Diabetes",
-    info: "Diabetes is a disease in which the body is unable to properly use and store glucose (a form of sugar). Glucose backs up in the bloodstream — causing one’s blood glucose (sometimes referred to as blood sugar) to rise too high."
+    info: "Diabetes is a disease in which the body is unable to properly use and store glucose (a form of sugar). Glucose backs up in the bloodstream, causing one’s blood glucose (sometimes referred to as blood sugar) to rise too high."
   },
   "Types of Diabetes": {
     next: "Type 1 Diabetes",
@@ -27,11 +27,11 @@ module.exports = {
   },
   "Gestational Diabetes": {
     next: "Eating habits",
-    info: "Gestational diabetes cause in pregnant women. But those are for temparory period can go away after child birth."
+    info: "Gestational diabetes is a condition in which women without previously diagnosed diabetes exhibit high blood glucose levels during pregnancy. It usually occurs for a temporary period and can go away after child birth."
   },
   "Eating habits": {
     next: "Leading cause of death",
-    info: "you can have sweets in moderation and as a plan of healthy meals plan."
+    info: "You can have sweets in moderation and should make of healthy meal plans."
   },
   "Leading cause of death": {
     next: "Heart Disease and Diabetes",
@@ -47,11 +47,11 @@ module.exports = {
   },
   "Insulin resistant diabetes": {
     next: "Side effects of diabetes",
-    info: "when you have type 2 diabetes, your cell don't respond to it like they should."
+    info: "When you have type 2 diabetes, your cells don't respond to insulin like they should."
   },
   "Side effects of diabetes": {
     next: "Facts of diabetes",
-    info:"When it happens, sugar backs up in your blood. Your pancreas makes even more insulin to get rid of sugar, so you wind up with too much of insulin and sugar both.",
+    info:"When it happens, sugar backs up in your blood. Your pancreas makes even more insulin to get rid of sugar, so you wind up with too much of both insulin and sugar.",
   },
   "Facts of diabetes": {
     next: "Symptoms",
@@ -59,61 +59,61 @@ module.exports = {
   },
   "Symptoms": {
     next: "More symptoms",
-    info: "Heavy thirst is common symptoms, people with high blood sugar are often extremely thirsty.Other symptoms include dry mouth, being hungry more (espicially after you eat).",
+    info: "Heavy thirst is a common symptom, people with high blood sugar are often extremely thirsty.Other symptoms include dry mouth and being hungry (espicially after you eat).",
   },
   "More symptoms": {
-    next: "Facts about begining of diabetes",
-    info: "peeing more, fatigue, blurred vision, headeahes, itching, numbness and tingling in your hands and feet, wounds heal more slowly, gain weight, frequent yeast infection, have sexual problems.",
+    next: "Facts about beginning of diabetes",
+    info: "Peeing more, fatigue, blurred vision, headaches, itching, numbness and tingling in your hands and feet, wounds heal more slowly, gain weight, frequent yeast infection, have sexual problems.",
   },
-  "Facts about begining of diabetes": {
+  "Facts about beginning of diabetes": {
     next: "Diabetes control",
-    info: "this disease doesn't start suddenly. some people are first diagnosed with prediabetes(also called impaired glucose tolerance or impaired fasting glucose).",
+    info: "This disease doesn't start suddenly. Some people are first diagnosed with prediabetes(also called impaired glucose tolerance or impaired fasting glucose).",
   },
   "Diabetes control": {
     next: "Initial treatment",
-    info: "lifestyle changes like weight loss, eating a healthier diet, getting more exercise can often ward off full blown diabetes. lifestyle changes is more important can cut your risk by 58%.",
+    info: "Lifestyle changes like weight loss, eating a healthier diet, and getting more exercise can often ward off full blown diabetes. Lifestyle changes is more important can cut your risk by 58%.",
   },
   "Initial treatment": {
     next: "Age at risk",
-    info: "Taking prescription medicine like metformin can help, talk to your doctor about treatment.",
+    info: "Taking prescription medicine like metformin can help. Talk to your doctor about treatment.",
   },
   "Age at risk": {
     next: "Main factor for diabetes",
-    info: "For 60 or older their risk of diabetes is 71%."
+    info: "For people age 60 or older their risk of diabetes is 71%."
   },
   "Main factor for diabetes": {
     next: "Severe effects of diabetes",
-    info: "Obesity is the main factor people likely to get diabetes"
+    info: "Obesity is the main factor for people likely to get diabetes."
   },
   "Severe effects of diabetes": {
     next: "Common effects on health",
-    info: "Over the time Blindness, heart disease, nerve and blood vessel damage, kidney disease and other major health problems",
+    info: "Over the time, severe effects of diabetes include blindness, heart disease, nerve and blood vessel damage, kidney disease and other major health problems.",
   },
   "Common effects on health": {
     next: "Cause of diabetes",
-    info: "Depression, gum disease also more common for diabetes patient as per CDC."
+    info: "Depression and gum disease are also more common for diabetes patients as per CDC."
   },
   "Cause of diabetes": {
     next: "Effects of type 1 diabetes",
-    info: "Cause can be genetic and lifestyle, affecting blood glucose level physical activity, illeness, stress"
+    info: "Causes can be genetic and lifestyle factors like physical activity, illeness, stress can affect blood glucose levels."
   },
   "Effects of type 1 diabetes": {
     next: "Type 1 diabetes treatment",
-    info: "Type 1 diabetes by an auto immune response that kills the body's own insulin producing cells"
+    info: "Type 1 diabetes by an auto immune response that kills the body's own insulin producing cells."
   },
   "Type 1 diabetes treatment": {
     next: "Reduce risk of Diabetes",
-    info: "Treatment of type 1 diabetes is taking insulin daily by injection or insulin pump"
+    info: "Treatment of type 1 diabetes is by taking insulin daily by injection or insulin pump."
   },
   "Reduce risk of Diabetes": {
     next: "Diagnostic test",
-    info: "By eating healthy meals and snacks and doing regular physical activity, regualar monitoring of blood glucose levels.Execise or physically active everyday of the week for 30 minutes, check feet every six month."
+    info: "You can reduce the risk of diabetes by eating healthy meals and snacks and doing regular physical activity and regular monitoring of blood glucose levels. Execise or be physically active every day of the week for 30 minutes, check feet every six months."
   },
   "Diagnostic test": {
     next: "More Diagnostic test",
-    info: "Blood test before meals targeted blood glucose level between 70 to 110 mg/dl and 2 hours after meals upto 140 mg/dl."
+    info: "Blood test before meals targeted blood glucose level between 70 to 110 mg/dl and 2 hours after meals up to 140 mg/dl."
   },
   "More Diagnostic test": {
     next: "Diabetes",
-    info: "A1C also know as average blood glucose level of 120 days should have targeted level below 7"
+    info: "A1C also know as average blood glucose level of 120 days should have targeted level below 7."
 };
